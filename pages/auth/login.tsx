fb1c5465af2832824b2b007f90e2f4527ea861ff
Prince Codes <princecodes247@gmail.com>
2022-03-07 15:42:44 +0100

    refactor: update auth pages

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -20,11 +20,7 @@ const Login: NextPage = () => {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-              alt="Workflow"
-            />
+            <p>Konan</p>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
             <p className="mt-2 text-center text-sm text-gray-600">
               Or{' '}
