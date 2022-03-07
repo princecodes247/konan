f6ba05e06b0773df4617e46e7222936bb0131e3c
Prince Codes <princecodes247@gmail.com>
2022-03-07 15:42:22 +0100

    feat: update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,12 +130,12 @@ function Hero() {
                       </a>
                     ))}
                   </div>
-                  <a
-                    href="#"
-                    className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
-                  >
+                  
+                  <Link href="/auth/login" passHref={true}>
+                    <span className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100">
                     Log in
-                  </a>
+                    </span>
+                  </Link>
                 </div>
               </Popover.Panel>
             </Transition>
@@ -144,8 +144,9 @@ function Hero() {
           <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Fund Raising</span>{' '}
-                <span className="block text-indigo-600 xl:inline">without stress!!</span>
+                <span className="block text-indigo-600 xl:inline">Decentralized</span>
+                {' '}
+                <span className="block xl:inline">Fund Raising</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -175,8 +176,8 @@ function Hero() {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          className="relative left-8 h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          src="/images/hero.jpg"
           alt=""
         />
       </div>
