f6a6a933dacd60dccc70a42efc84acf03d369a2a
Prince Codes <princecodes247@gmail.com>
2022-03-26 09:47:24 +0100

    feat: Create Loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+      <div className="bg-white">
+          <div className="circle"></div>
+          <p>Loading...</p>
+      </div>
+    )
+  }
+  
